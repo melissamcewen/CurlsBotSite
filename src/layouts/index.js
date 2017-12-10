a856cfaaa5dac5cefcd555d5543911217aa6f68b
Melissa Mcewen <melissa@equivoice.com>
2017-12-09 21:48:47 -0600

    trying react google analytics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import logo from '../images/logo-small.png'
+import ReactGA from 'react-ga'
 
 import './index.css'
 
@@ -43,7 +44,17 @@ const TemplateWrapper = ({ children }) => (
         { name: 'description', content: 'A hair care chatbot!' },
         { name: 'keywords', content: 'chatbots, curly girl' },
       ]}
-    />
+    >
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110983103-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-110983103-1');
+</script>
+
+     </Helmet>
     <Header />
     <div
       style={{
