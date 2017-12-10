f1490186b070048da0cefa8b6dba5f8f9095bfce
Melissa Mcewen <melissa@equivoice.com>
2017-12-09 22:14:48 -0600

    adding google analyrics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -36,41 +36,52 @@ const Header = () => (
   </div>
 )
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="CurlsBot"
-      meta={[
-        { name: 'description', content: 'A hair care chatbot!' },
-        { name: 'keywords', content: 'chatbots, curly girl' },
-      ]}
-    >
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110983103-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-110983103-1');
-</script>
-
-     </Helmet>
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+const logPageView = (ReactGA) => {
+  console.log("Logged view on: ", window.location.pathname)
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
 }
 
-export default TemplateWrapper
+
+export default class TemplateWrapper extends React.Component {
+  static propTypes = {
+    children: PropTypes.func,
+  };
+
+  componentDidMount() {
+    ReactGA.initialize('UA-110983103-1');
+    logPageView(ReactGA)
+  }
+
+  componentWillReceiveProps() {
+    logPageView(ReactGA);
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="CurlsBot"
+          meta={[
+            { name: 'description', content: 'A hair care chatbot!' },
+            { name: 'keywords', content: 'chatbots, curly girl' },
+          ]}
+        />
+        <Header />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {this.props.children()}
+        </div>
+      </div>
+    );
+  }
+}
+
+
