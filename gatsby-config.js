1823436ed05059803f765d4dad071d8003db3d85
Melissa Mcewen <mgmcewen@gmail.com>
2018-06-28 21:19:58 -0500

    adding PWA features

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,22 @@ module.exports = {
 
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sitemap`
+  `gatsby-plugin-react-helmet`,
+  `gatsby-plugin-sitemap`,
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: "CurlsBot",
+      short_name: "Curlsbot",
+      start_url: "/",
+      background_color: "#E7D467",
+      theme_color: "#E7D467",
+      display: "minimal-ui",
+      icon: "src/images/icon.png"
+    }
+  },
+  'gatsby-plugin-offline'
 
   ],
 }
+ 
