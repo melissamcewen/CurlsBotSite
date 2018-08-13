e5fe7fcffa72f02d849b1b73b59722bb2a901921
Melissa Mcewen <mgmcewen@gmail.com>
2018-08-12 19:28:27 -0500

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-2a25b4c2f67b6022a976.js","4cf33372c81a5d886b48d68aaeff9568"],["/app-6c64c496fde1cb76abdb.js","1f4da978a79c121b5de027177cef1c51"],["/commons-e6161044c8acc044bfdb.js","ef5c1b874a265328e4b7f112085e8db5"],["/index.html","5c2b03f6b1440e24c9befc80f46b8e62"],["/manifest.webmanifest","09e365d111743ad0a46486ba458cf6a9"],["/offline-plugin-app-shell-fallback/index.html","157a63dbb3ce88e309b5f263dadfe090"]];
+var precacheConfig = [["/app-2a25b4c2f67b6022a976.js","4cf33372c81a5d886b48d68aaeff9568"],["/app-6c64c496fde1cb76abdb.js","1f4da978a79c121b5de027177cef1c51"],["/commons-e6161044c8acc044bfdb.js","ef5c1b874a265328e4b7f112085e8db5"],["/index.html","17575e57143689a3aba08d1d542e7d98"],["/manifest.webmanifest","09e365d111743ad0a46486ba458cf6a9"],["/offline-plugin-app-shell-fallback/index.html","729ec5f6dea087390c222c944ec2e8b2"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
