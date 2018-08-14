7f1ce1283d4426005528bd38e7fc4010973f0f4a
Melissa Mcewen <mgmcewen@gmail.com>
2018-08-14 18:05:34 -0500

    Updates

---
+++ path---shampoo-bars-are-not-cg-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xc8d67d55ad33],{"./node_modules/json-loader/index.js!./.cache/json/shampoo-bars-are-not-cg.json":function(o,n){o.exports={pathContext:{}}}});
+//# sourceMappingURL=path---shampoo-bars-are-not-cg-a0e39f21c11f6a62c5ab.js.map
