4a344a954f3c0da758751080cb2cd8ffd90b75d7
Melissa McEwen <mgmcewen@gmail.com>
2024-12-21 07:26:25 -0600

    updating social links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,8 +13,9 @@ export default function ContactPage() {
 
           <div className="prose prose-lg max-w-none">
             <p>
-              Have questions about your hair care journey? Want to report an ingredient analysis issue?
-              We&apos;re here to help! Here are the best ways to reach us:
+              Have questions about your hair care journey? Want to report an
+              ingredient analysis issue? We&apos;re here to help! Here are the
+              best ways to reach us:
             </p>
 
             <h2>Email</h2>
@@ -26,35 +27,64 @@ export default function ContactPage() {
             </p>
 
             <h2>Social Media</h2>
-            <p>
-              Follow us and join the conversation:
-            </p>
+            <p>Follow us and join the conversation:</p>
             <ul>
               <li>
-                <a href="#" className="link link-primary">Facebook</a> - Join our community discussions
+                <a
+                  href="https://www.facebook.com/groups/1804576666517325"
+                  className="link link-primary"
+                >
+                  Facebook
+                </a>{' '}
+                - Join our community discussions
               </li>
               <li>
-                <a href="#" className="link link-primary">Instagram</a> - See hair care tips and product reviews
+                <a
+                  href="https://www.instagram.com/curlsbot"
+                  className="link link-primary"
+                >
+                  Instagram
+                </a>{' '}
+                - See hair care tips and product reviews
               </li>
               <li>
-                <a href="#" className="link link-primary">Reddit</a> - Participate in detailed hair care discussions
+                <a
+                  href="https://www.reddit.com/r/curlsbot/"
+                  className="link link-primary"
+                >
+                  Reddit
+                </a>{' '}
+                - Participate in detailed hair care discussions
               </li>
               <li>
-                <a href="#" className="link link-primary">Discord</a> - Chat with other curly hair enthusiasts
+                <a
+                  href="https://discord.gg/3AkQqkADv9"
+                  className="link link-primary"
+                >
+                  Discord
+                </a>{' '}
+                - Chat with other curly hair enthusiasts
               </li>
               <li>
-                <a href="https://twitter.com/curlsbot" className="link link-primary">Twitter</a> - Get the latest updates
+                <a
+                  href="https://twitter.com/curlsbot"
+                  className="link link-primary"
+                >
+                  Twitter
+                </a>{' '}
+                - Get the latest updates
               </li>
             </ul>
 
             <h2>Open Source</h2>
             <p>
-              CurlsBot is open source! If you&apos;re a developer and want to contribute:
+              CurlsBot is open source! If you&apos;re a developer and want to
+              contribute:
             </p>
             <ul>
               <li>
                 <a
-                  href="https://github.com/melissamcewen/CurlsBotSite"
+                  href="https://github.com/melissamcewen/CurlsBotAPI"
                   className="link link-primary"
                 >
                   Visit our GitHub repository
@@ -64,11 +94,10 @@ export default function ContactPage() {
 
             <h2>Business Inquiries</h2>
             <p>
-              For business-related questions, sponsorship opportunities, or other inquiries,
-              please reach out through any of our social media channels.
+              For business-related questions, sponsorship opportunities, or
+              other inquiries, please reach out through any of our social media
+              channels.
             </p>
-
-
           </div>
         </div>
       </div>
