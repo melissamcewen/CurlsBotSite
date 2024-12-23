4d851b6cd4bd2320d2a176295f960c1759a18d8d
Melissa McEwen <mgmcewen@gmail.com>
2024-12-22 22:47:44 -0600

    fixing ga tag

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,11 +3,8 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Nav from '../components/nav'
-import ReactGA from 'react-ga'
 import { withPrefix } from 'gatsby-link'
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
-
-
 
 const Header = () => (
   <div
@@ -23,16 +20,13 @@ const Header = () => (
         padding: '1rem 1.0875rem',
       }}
     >
-
       <Nav/>
     </div>
   </div>
 )
 
-
 const Footer = () => (
-  <div
-  >
+  <div>
     <div
       style={{
         margin: '0 auto',
@@ -40,51 +34,37 @@ const Footer = () => (
         padding: '1rem 1.0875rem',
       }}
     >
-
       <small>This web site is not endorsed by, directly affiliated with, maintained, authorized, or sponsored by The Curly Girl Method by Lorraine Massey™️. Some links on Curlsbot are Amazon Affiliate links. Shopping through these links supports the further development of Curlsbot.</small>
     </div>
   </div>
 )
-
-
-const logPageView = (ReactGA) => {
-  console.log("Logged view on: ", window.location.pathname)
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
-}
-
 
 export default class TemplateWrapper extends React.Component {
   static propTypes = {
     children: PropTypes.func,
   };
 
-  componentDidMount() {
-    ReactGA.initialize('G-BHJR6M5MLQ');
-    logPageView(ReactGA)
-  }
-
-  componentWillReceiveProps() {
-    logPageView(ReactGA);
-  }
-
   render() {
     return (
       <div>
-        <Helmet
-          title="CurlsBot"
-          meta={[
-            { name: 'description', content: 'A hair care chatbot!' },
-            { name: 'keywords', content: 'chatbots, curly hair' },
-            {
-              property: 'og:image',
-              content: 'http://www.curlsbot.com/img/icon.png'
-            }
-          ]}
-        >
+        <Helmet>
+          <title>CurlsBot</title>
+          <meta name="description" content="A hair care chatbot!" />
+          <meta name="keywords" content="chatbots, curly hair" />
+          <meta property="og:image" content="http://www.curlsbot.com/img/icon.png" />
           <meta name="google-site-verification" content="vFMnYOqnsQwevkYo--zeevG2gat6gN-QAqbauxy1N7A" />
 
-      </Helmet>
+          {/* Google Analytics 4 tag */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-BHJR6M5MLQ"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-BHJR6M5MLQ');
+            `}
+          </script>
+        </Helmet>
         <Header />
         <div
           style={{
@@ -95,7 +75,6 @@ export default class TemplateWrapper extends React.Component {
             clear: 'both'
           }}
         >
-
           {this.props.children()}
         </div>
         <Footer />
