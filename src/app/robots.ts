20d28e0dfe5aeb13ec2b41ae223bb78402e13ea7
Melissa McEwen <mgmcewen@gmail.com>
2025-06-16 15:14:47 -0500

    updating robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,11 +7,7 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: '*',
         allow: '/',
         disallow: ['/api/', '/_next/', '/static/'],
-      },
-      {
-        userAgent: 'GPTBot',
-        disallow: '/',
-      },
+      }
     ],
     sitemap: 'https://www.curlsbot.com/sitemap.xml',
     host: 'https://www.curlsbot.com',
