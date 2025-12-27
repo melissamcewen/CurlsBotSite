f57c4355c1dbc4e7d801ce7c5e3da8797a69987b
Melissa McEwen <mgmcewen@gmail.com>
2025-12-26 18:23:15 -0600

    adding table

--- src/components/mdx/PorosityDefinitionsTable.tsx
+++ src/components/mdx/PorosityDefinitionsTable.tsx
@@ -2,65 +2,87 @@ import React from 'react';
 
 export default function PorosityDefinitionsTable() {
   return (
-    <div className="overflow-x-auto rounded-2xl my-6 not-prose">
-      <table className="table table-zebra w-full bg-base-100">
+    <div className="overflow-x-auto rounded-2xl my-8 not-prose border border-base-300 bg-base-100">
+      <table className="table w-full">
         <thead>
-          <tr>
-            <th className="font-semibold">Porosity Type</th>
-            <th className="font-semibold">Damage Definition</th>
-            <th className="font-semibold">Internet Definition</th>
-            <th className="font-semibold">Future Science-based Definition?</th>
+          <tr className="bg-base-200">
+            <th className="font-semibold text-base py-4 px-4 align-top">
+              Porosity Type
+            </th>
+            <th className="font-semibold text-base py-4 px-4 align-top">
+              Damage Definition
+            </th>
+            <th className="font-semibold text-base py-4 px-4 align-top">
+              Internet Definition
+            </th>
+            <th className="font-semibold text-base py-4 px-4 align-top">
+              Future Science-based Definition?
+            </th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="font-medium">Low porosity</td>
-            <td>
+          <tr className="border-b border-base-300">
+            <td className="font-semibold text-base-content py-5 px-4 align-top bg-base-50">
+              Low porosity
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Healthy hair, can use any products without worrying about damage,
               has optimal moisture naturally
             </td>
-            <td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Water and product resistant hair, sensitive to protein, needs
               special treatments to get moisture in, water based products and
               &quot;light&quot; oils, needs clarifying
             </td>
-            <td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               STILL healthy hair, but perhaps treatment and humidity resistant
               due to cuticle thickness, lipids, strand thickness, curvature
             </td>
           </tr>
-          <tr>
-            <td className="font-medium">Normal Porosity</td>
-            <td>Same as low porosity</td>
-            <td>Healthy hair, can pretty much use anything</td>
-            <td>
+          <tr className="border-b border-base-300">
+            <td className="font-semibold text-base-content py-5 px-4 align-top bg-base-50">
+              Normal Porosity
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
+              Same as low porosity
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
+              Healthy hair, can pretty much use anything
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               ALSO healthy hair but less treatment and humidity resistant
               because of cuticle thickness, lipids, strand thickness, curvature
             </td>
           </tr>
-          <tr>
-            <td className="font-medium">High porosity</td>
-            <td>
+          <tr className="border-b border-base-300">
+            <td className="font-semibold text-base-content py-5 px-4 align-top bg-base-50">
+              High porosity
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Damaged hair, can benefit from treatments (bond repair, protein),
               increased water resistance, cationic conditioners
             </td>
-            <td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Easily soaked by water and products, benefits from oils/butters,
               &quot;seal&quot; in moisture to prevent escaping
             </td>
-            <td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Damaged hair, can benefit from treatments (bond repair, protein),
               increased water resistance, cationic conditioners
             </td>
           </tr>
           <tr>
-            <td className="font-medium">Mixed porosity</td>
-            <td>
+            <td className="font-semibold text-base-content py-5 px-4 align-top bg-base-50">
+              Mixed porosity
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Most people have lower porosity towards their roots and higher
               towards the ends. The longer the hair, the higher the porosity
             </td>
-            <td>-</td>
-            <td>
+            <td className="py-5 px-4 align-top leading-relaxed text-base-content/60">
+              -
+            </td>
+            <td className="py-5 px-4 align-top leading-relaxed">
               Most people have lower porosity towards their roots and higher
               towards the ends. The longer the hair, the higher the porosity
             </td>
